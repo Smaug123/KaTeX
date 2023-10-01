eab81b4f62d099da9c25b9e6490447085fee9655
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-10-01 09:59:01 +0100

    Add fonts

--- flake.nix
+++ flake.nix
@@ -13,6 +13,16 @@
     let pkgs = nixpkgs.legacyPackages.${system}; in
     {
         packages.default = (import ./default.nix) { inherit pkgs; };
+        packages.fonts = pkgs.stdenv.mkDerivation {
+            __contentAddressed = true;
+            pname = "katex-fonts";
+            version = "0.1.0";
+            src = ./fonts;
+            installPhase = ''
+              mkdir -p "$out"
+              cp -r ./. $out
+            '';
+        };
     }
     );
 }
